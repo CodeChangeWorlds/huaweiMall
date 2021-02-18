240c1bc1d7c240f69bdf883f1c5ad49435fbb41d
CodeChangeWorlds <1450911745@qq.com>
2021-02-18 10:26:35 +0800

    完成首页

---
+++ js/index.js
@@ -0,0 +1,18 @@
+// jq read方法简写
+$(function () {
+  $('#nav').load('../views/nav.html')
+  $('#header').load('../views/header.html')
+  $('#main').load('../views/main.html')
+  $('#foot').load('../views/footer.html')
+  $(window).scroll(function () {
+    if ($(window).scrollTop() >= 500) {
+      $('#hungBar-top').fadeIn();
+    }
+    else {
+      $('#hungBar-top').fadeOut();
+    }
+  })
+  $('#hungBar-top').click(function () {
+    $('html,body').animate({ scrollTop: 0 }, 500);
+  })
+})
